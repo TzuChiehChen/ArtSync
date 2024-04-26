d12fec85edf8088cac797bd48c6caf862d0ab646
TzuChiehChen <tzc005@ucsd.edu>
2024-04-25 19:03:14 -0700

    heardre wrapper

--- artsync/src/Components/Header/Header.jsx
+++ artsync/src/Components/Header/Header.jsx
@@ -3,13 +3,16 @@ import "./Header.css"
 
 const Header = () => {
   return (
-    <header> <div className="wrapper">
-      <h1>ArtSync</h1>
-      <p className="Title"> Where Musicians and Designers Sync Creativity</p>
-      <a href = "#" className = "ArtSYNC CCCC">
-        artsync figma
-      </a>
-    </div>
+    <header>
+      <div className="wrapper">
+        <div className="cta">
+          <h1>ArtSync</h1>
+          <p className="Title"> Where Musicians and Designers Sync Creativity</p>
+          <a href="#" className="ArtSYNC CCCC">
+            artsync figma
+          </a>
+        </div>
+      </div>
     </ header>
   )
 }
